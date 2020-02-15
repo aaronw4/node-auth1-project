2c8d671110e34cc8e3469a4dc9b561a650c145b0
Aaron Williams <aaronrw4@yahoo.com>
2020-02-14 17:23:19 -0800

    Added session and options to server.js

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const session = require('express-session');
 
 const usersRouter = require('./users/usersRouter');
+
+const sessionOptions = {
+    name: "userCookie",
+    secret: "secretCookie",
+    cookie: {
+        maxAge: 1000 * 60 * 60,
+        secure: false,
+        httpOnly: true
+    },
+    resave: false,
+    saveUninitialized: false
+}
 
 const server = express();
 
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
+server.use(session(sessionOptions));
+
 server.use('/users', usersRouter);
 
 module.exports = server;
