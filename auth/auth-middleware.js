226044833b217ccbdf413710330c6cca95131924
Aaron Williams <aaronrw4@yahoo.com>
2020-02-14 17:34:23 -0800

    Adjusted auth-middleware for sessions

--- auth/auth-middleware.js
+++ auth/auth-middleware.js
@@ -1,28 +1,11 @@
 const users = require('../users/usersModel');
 const bcrypt = require('bcryptjs');
 
-module.exports = function auth(req, res, next) {
-    const {username, password} = req.headers;
-
-    if (username && password) {
-        users.findBy({username})
-            .first()
-            .then(user => {
-                if (user && bcrypt.compareSync(password, user.password)) {
-                    next();
-                } else {
-                    res.status(401).json({
-                        error: 'Invalid Credentials.'
-                    })
-                }
-            })
-            .catch(err => {
-                res.status(400).json({
-                    error: 'Please provide valid credentials.'
-                })
-            })
+module.exports = function auth(req, res, next) {   
+    if (req.session && req.session.user) {
+       next();
     } else {
-        res.status(400).json({
+        res.status(401).json({
             error: 'Please provide valid credentials.'
         })
     }
